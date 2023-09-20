2cd0a992042074e5b01487b195e582b13b0e0414
mumtaz mohammed <mumtazmohammed08@gmail.com>
2023-09-20 12:03:56 +0200

    created nested routing in react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import { Link, Outlet } from 'react-router-dom';
 
-function App() {
+function Home() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello Netherlands!!</h1>
+      <nav>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+      </nav>
     </div>
   );
 }
 
-export default App;
+
+export function About(){
+  return(
+    <div>
+      <h2>About us</h2>
+      <Outlet />
+    </div>
+  );
+}
+
+export function History(){
+  return(
+    <div>
+      <h2>About history</h2>
+    </div>
+  );
+}
+
+
+export function Contact(){
+  return(
+    <div>
+      <h2>Contact us</h2>
+    </div>
+  );
+}
+
+ export function App() {
+  return (
+   <Home />
+  );
+}
---
+++ src/functions.test.js
@@ -0,0 +1,5 @@
+import {timesTwo} from "./functions.js";
+
+test("multiples of two", () => {
+  expect(timesTwo(2)).toBe(4);
+});
